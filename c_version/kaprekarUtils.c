8995a6ec8d1bb584d97f6f184450195844006cee
Kalen <31481852+kalenwallin@users.noreply.github.com>
2019-09-09 20:23:03 -0500

    Removed numberOfDigits from int list, fixed spelling on numberOfDigits to numDigits

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -28,7 +28,7 @@ int isKaprekar(int n) {
     return 0; //changed false to 0
   }
 
-  int i, numberOfDigits; //defined numberOfDigits as an integer
+  int i;
   //compute the square
   long square = n * (long) n;
   //compute the number of digits of the square
@@ -37,7 +37,7 @@ int isKaprekar(int n) {
   long first, second;
 
   //for each possible "split" of the square...
-  for(i=1; i<=numberOfDigits; i++) {
+  for(i=1; i<=numDigits; i++) {
     //increase the modulus by a factor of 10
     modulous *= 10;
 
