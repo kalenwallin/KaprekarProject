cbb3b5f04283e1c6df6106904f633da0fdc5a3e3
Kalen <31481852+kalenwallin@users.noreply.github.com>
2019-09-11 16:18:37 -0500

    Fixed spelling on all modulous to modulus

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -33,17 +33,17 @@ int isKaprekar(int n) {
   long square = n * (long) n;
   //compute the number of digits of the square
   int numDigits = (int) log10(n) + 1;
-  long modulous = 0; //fixed spelling on modulus to modulous
+  long modulus = 0;
   long first, second;
 
   //for each possible "split" of the square...
   for(i=1; i<=numDigits; i++) {
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;
 
     //split the square into two parts
-    first = square / modulous; //fixed spelling on modulus to modulous
-    second = square % modulous; //fixed spelling on modulus to modulous
+    first = square / modulus;
+    second = square % modulus;
 
     //test if the split makes a Kaprekar number
     //the second part cannot be zero and if
